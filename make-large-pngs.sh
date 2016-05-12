f61cefe3f58b6ffdbb7f61195ebe992cb91ef790
Joshua Marsh (icub3d) <joshua@themarshians.com>
2016-05-12 09:18:29 -0600

    svg of logo now uses a viewbox and classes so they can be styles with css and the size adjusted in html

--- make-large-pngs.sh
+++ make-large-pngs.sh
@@ -3,7 +3,7 @@
 for i in $(ls palettes); do
     mkdir -p dist/$i/images
     css="`fgrep -e default-primary-color -e accent-color palettes/$i/palette.css | 
-          sed -e 's/.default-primary-color/#pants/g' -e 's/.accent-color/#dingleberry/g' -e 's/background/fill/g'`"
+          sed -e 's/.default-primary-color/.dinglebit-logo-pants/g' -e 's/.accent-color/.dinglebit-logo-dingleberry/g' -e 's/background/fill/g'`"
     nodejs node_modules/svgexport/bin/index.js dinglebit.svg dist/$i/images/dinglebit.png "$css"
 done
 
